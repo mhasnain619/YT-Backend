d788433ad213c445d774cd010548e1dbcfa8a1e0
Muhammad Hasnain <hasnain0893@gmail.com>
2025-05-21 01:40:56 +0500

    update

--- Src/Db/database.js
+++ Src/Db/database.js
@@ -4,9 +4,9 @@ import { DB_NAME } from "../constants.js";
 const connectDB = async () => {
     try {
         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-        console.log("✅ MongoDB connected");
+        console.log("MongoDB connected");
     } catch (error) {
-        console.log("❌ MongoDB connection error:", error);
+        console.log("MongoDB connection error:", error);
         process.exit(1);
     }
 };
