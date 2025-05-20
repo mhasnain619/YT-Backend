7c9ff34fd2736835a3ff1e00ae24119f23e239ba
Muhammad Hasnain <hasnain0893@gmail.com>
2025-05-21 00:51:36 +0500

    update

--- Src/index.js
+++ Src/index.js
@@ -1,9 +1,18 @@
 import express from "express";
-import connectDB from "./Db/index.js";
+import connectDB from "./Db/database.js";
 import dotenv from "dotenv";
 dotenv.config();
 
 const app = express()
 
 
-connectDB()
+connectDB().then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`server started at port : ${process.env.PORT}`);
+
+    })
+})
+    .catch((error) => {
+        console.log('mongo db connection failed', error);
+
+    })
